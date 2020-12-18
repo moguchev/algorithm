ddc1c5424e667d0da2e9b6d02509e8170ebd089e
= <abracadabra.14@maul.ru>
2020-12-18 20:52:11 +0300

    Взлом RSA

--- arithmetic/comparison_arithmetic.go
+++ arithmetic/comparison_arithmetic.go
@@ -1,6 +1,10 @@
 package arithmetic
 
-import "math/rand"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
 // ModExp return x^y mod N
 func ModExp(x, y, N int64) int64 {
@@ -46,13 +50,50 @@ func Primality2(N int64) bool {
 	k := 20
 	random := make([]int64, 0, k)
 	for i := 0; i < k; i++ {
-		random = append(random, rand.Int63n(N))
+		rnd := rand.Int63n(N)
+		if rnd > 1 {
+			random = append(random, rnd)
+		} else {
+			i--
+		}
 	}
 
-	isPrim := true
 	for i := range random {
-		isPrim = isPrim && (ModExp(random[i], N-1, N) == 1)
+		if ModExp(random[i], N-1, N) != 1 {
+
+		}
 	}
 
-	return isPrim
+	return true
+}
+
+// BreakeRSA //
+func BreakeRSA(N, e, C int64) (p, q, M int64) {
+	fmt.Println(int64(math.Sqrt(float64(N))))
+	var i int64
+	for i = 2; i <= int64(math.Sqrt(float64(N))); i++ {
+		if N%i == 0 {
+			if Primality2(i) {
+				p = i
+				q = N / p
+				fmt.Printf("p = %d, q = %d\n", p, q)
+			}
+		}
+	}
+
+	fin := (p - 1) * (q - 1)
+	fmt.Printf("fi(n) = %d\n", fin)
+
+	d, _, _ := ExtendedEuclid(e, fin)
+	if d < 0 {
+		d += fin
+	}
+	fmt.Printf("d = %d\n", d)
+
+	M = ModExp(C, d, N)
+	fmt.Printf("M = %d\n", M)
+
+	fmt.Printf("Binary M: %b\n", M)
+
+	return p, q, M
 }
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/moguchev/algorithm/arithmetic"
+
+func main() {
+	// N, e, C
+	arithmetic.BreakeRSA(10573, 8483, 9192)
+}
